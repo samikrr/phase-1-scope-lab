dacb34e60d191e28d7380dd21bb87c721b85246b
Samuel Kirori <samuelkirori300@gmail.com>
2024-04-03 09:31:09 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+//declare customername variable in globalscope
+var customerName="bob";
+
+//modify customername to uppercase
+function upperCaseCustomerName(){
+  customerName=customerName.toUpperCase()
+  console.log(customerName);
+}
+
+// declaring a variable in global scope from inside a function 
+
+function setBestCustomer(){
+    console.log(bestCustomer="not bob");
+}
+
+//function tries to changes the best customer variable
+function overwriteBestCustomer(){
+    console.log(bestCustomer="maybe bob");
+}
+//declaring a const in global scope,assign initial value.
+//function to try to change const
+const LeastFavoriteCustomer="Wanjira"
+function changeLeastFavoriteCustomer(){
+    console.log(LeastFavoriteCustomer="Linda")
+}
